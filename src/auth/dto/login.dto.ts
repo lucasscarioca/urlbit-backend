f30a090a26256ef01d34fce4b7ec9344e3187cfe
Lucas Carioca <lucascarioca97@gmail.com>
2023-02-09 21:07:08 -0300

    template project cleanup

--- src/auth/dto/login.dto.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const loginSchema = z.object({
-  email: z.string().trim().email(),
-  password: z.string(),
-})
-
-export type LoginInput = z.infer<typeof loginSchema>
--- src/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import authRouter from './auth/auth.routes'
-
-const router = Router()
-
-router.use('/sessions', authRouter)
-
-export { router }
